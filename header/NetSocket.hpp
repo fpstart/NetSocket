091ddf5780781d09b83085faad52dd0cf2059cf9
fpstart <123644185+fpstart@users.noreply.github.com>
2023-08-21 22:14:22 +0800

    update header/NetSocket.hpp

--- header/NetSocket.hpp
+++ header/NetSocket.hpp
@@ -10,12 +10,26 @@ namespace FRPC
   class NetSocket
   {
     public:
+      NetSocket();
+      NetSocket(sockaddr_in6 addr);
       bool setConfig(bool isUDP,bool isServer);
       bool setAddr(sockaddr_in6 addr);
       bool setAddr(in_addr6 addr,unsigned short port);
       //bool setAddr(char* addr,unsigned short port);
       //bool setAddr(char* addr);
       bool open();
+      class AcceptObj
+      {
+        public:
+          read(char* text,const int len);
+          write(char* text,const int len);
+        private:
+          int linkfd;
+          sockaddr_in6 address;
+      };
+      AcceptObj accept();
     private:
+      int socketfd;
+      sockaddr_in6 address;
   };
 };
