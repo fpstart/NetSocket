ee024ee0ee300495b96c31aa321d8c1f452cfd84
fpstart <123644185+fpstart@users.noreply.github.com>
2023-08-21 22:05:38 +0800

    update header/NetSocket.hpp

--- header/NetSocket.hpp
+++ header/NetSocket.hpp
@@ -1,3 +1,10 @@
+#include <unistd.h>
+#include <iostream>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
 namespace FRPC
 {
   class NetSocket
@@ -5,5 +12,10 @@ namespace FRPC
     public:
       bool setConfig(bool isUDP,bool isServer);
       bool setAddr(sockaddr_in6 addr);
+      bool setAddr(in_addr6 addr,unsigned short port);
+      //bool setAddr(char* addr,unsigned short port);
+      //bool setAddr(char* addr);
+      bool open();
+    private:
   };
 };
