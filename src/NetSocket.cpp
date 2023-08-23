4d4a21bef985ef255614b8f140b831020b4921d8
fpstart <123644185+fpstart@users.noreply.github.com>
2023-08-23 19:50:19 +0800

    update src/NetSocket.cpp

--- src/NetSocket.cpp
+++ src/NetSocket.cpp
@@ -1,5 +1,11 @@
 #include <../header/NetSocket.hpp>
 bool FPRC::NetSocket::setConfig(bool isUDP,bool isServer)
 {
-  
+  this.Protocol = isUDP;
+  this.Way = isServer;
+  return true;
+}
+bool FPRC::NetSocket::setAddr(sockaddr_in6 addr)
+{
+  this.address = addr;
 }
