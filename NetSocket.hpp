0f41b4860b86f2627f5550807c1e4416bec26553
fpstart <123644185+fpstart@users.noreply.github.com>
2023-11-03 13:36:40 +0800

    update NetSocket.hpp

--- NetSocket.hpp
+++ NetSocket.hpp
@@ -2,5 +2,9 @@ namespace zeroref{
   class SocketObj;
   class BasicSocket{
     friend class SocketObj;
-  }
+    protected:
+    virtual void mSendMsg(&SocketObj sobj);
+    virtual void mWriteMsg(&SocketObj sobj);
+  };
+  class TCPSocketServer : public BasicSocket{}
 }
