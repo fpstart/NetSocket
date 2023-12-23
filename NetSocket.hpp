0d6d2e582e08f08b94428b4abfa58563dde91d25
fpstart <123644185+fpstart@users.noreply.github.com>
2023-12-23 18:54:02 +0800

    update NetSocket.hpp

--- NetSocket.hpp
+++ NetSocket.hpp
@@ -1,4 +1,13 @@
 #ifndef __SOCKET_ZEROSRP_H__
-
+#include <string>
+namespace ZSRP
+{
+  class BasicSocketIO
+  {
+    ///所有Socket类的父类，提供泛型支持
+    protected:
+      virtual void sendMsg(string buf) = 0;
+  }；
+};
 #define __SOCKET_ZEROSRP_H__
 #endif
