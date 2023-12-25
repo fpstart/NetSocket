b36555ae86d37c1db28ce3cc00feef2eeb9a00b1
fpstart <123644185+fpstart@users.noreply.github.com>
2023-12-25 22:35:23 +0800

    update NetSocket.hpp

--- NetSocket.hpp
+++ NetSocket.hpp
@@ -1,4 +1,6 @@
 #ifndef __SOCKET_ZEROSRP_H__
+#define SOCKET_TYPE int
+#define SOCKET_ADDR sockaddr_in6
 #include <string>
 namespace ZSRP
 {
@@ -6,9 +8,22 @@ namespace ZSRP
   {
     ///所有Socket类的父类，提供泛型支持
     protected:
-      /** @brief 由子类重写，提供多态性
-        @prama[in] buf 发送的数据*/
-      virtual void sendMsg(string& buf) = 0;
+      struct SocketInfo
+      {
+        ///套接字信息
+        SOCKET_TYPE fd;
+        SOCKET_ADDR ad;
+      };
+      /** 
+       *@brief 由子类重写，提供多态性
+       *@prama[in] buf 发送的数据
+       */
+      virtual void writeMsg(SInfo obj,string& buf) = 0;
+      /** 
+       *@brief 由子类重写，提供多态性
+       *@prama[out] buf 接受的数据
+       */
+      virtual void readMsg(SInfo obj,string& buf) = 0;
   }；
 };
 #define __SOCKET_ZEROSRP_H__
